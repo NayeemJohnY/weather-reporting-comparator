a9008789403e8cc4a1a5ed1d8d258f60d274b6e2
John <nayeem.john@cengage.com>
2020-06-19 18:07:46 +0530

    Weather constants interface

---
+++ src/main/java/base/WeatherConstants.java
@@ -0,0 +1,10 @@
+package base;
+
+public interface WeatherConstants {
+
+	String TEMP_IN_DEGREES = "Temp in Degrees";
+	String TEMP_IN_FAHRENHEIT = "Temp in Fahrenheit";
+	String HUMIDITY = "Humidity";
+	String WIND = "Wind";
+	String CONDITION = "Condition";
+}
