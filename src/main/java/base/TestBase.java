950a6eccd296b38faa88c24f409296d36d914f7e
John <nayeem.john@cengage.com>
2020-06-19 18:09:56 +0530

    Base class with BeforeClass config & Dataprovider for mutliple test

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -1,17 +1,104 @@
 package base;
+
+import static utils.ExtentHTMLReporter.reportLog;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 
 import utils.ExtentHTMLReporter;
 import utils.TestProperties;
 
 public class TestBase {
-	
-	protected final String URL = TestProperties.getProperties("URL");
+
+	protected String url = TestProperties.getProperties("URL");
 	protected Logger log = LogManager.getLogger();
-	protected ExtentHTMLReporter extentReporter = new ExtentHTMLReporter();
+	protected static ExtentHTMLReporter extentReporter  = new ExtentHTMLReporter();;
 	protected ExtentTest extentTest;
+	protected String searchCity = System.getProperty("city");
+	protected double variance = 0;
+	protected String weatherConditionKey = "";
+
+	/**
+	 * Method to set up start a report, weather condition key & variance
+	 * @param testCaseName
+	 * @param testDescription
+	 */
+	@BeforeClass
+	@Parameters({ "testCaseName", "testDescription" })
+	public void beforeClassMethod(String testCaseName, String testDescription) {
+		extentTest = extentReporter.startTestCase(testCaseName, testDescription);
+		if (testCaseName.contains("Temperature In Degress")) {
+			weatherConditionKey = WeatherConstants.TEMP_IN_DEGREES;
+			setupVariance("Temperature_variance");
+		} else if (testCaseName.contains("Temperature In Fahrenheit")) {
+			weatherConditionKey = WeatherConstants.TEMP_IN_FAHRENHEIT;
+			setupVariance("Temperature_variance");
+		} else if (testCaseName.contains("Humidity")) {
+			weatherConditionKey = WeatherConstants.HUMIDITY;
+			setupVariance("Humidity_variance");
+		}
+	}
+
+	/**
+	 * Method to data of cities for Test
+	 * @return Object[] - Array of Objects of cities
+	 */
+	@DataProvider(name = "Search City Names")
+	public Object[] getCityNames() {
+		if (searchCity == null || searchCity.isEmpty()) {
+			searchCity = TestProperties.getProperties("city");
+			reportLog(extentTest, Status.WARNING,
+					"Search City is not provided, checking for default cities : <b>" + searchCity + "</b>");
+		} else {
+			reportLog(extentTest, Status.PASS, "Search City is: " + searchCity);
+		}
+		return searchCity.split(",");
+	}
+
+	/**
+	 * Method to check the difference of values in specified variance range
+	 * @param valueString1
+	 * @param valueString2
+	 * @return boolean true if difference in specified range;
+	 * @throws IllegalStateException with Matcher Exception
+	 */
+	protected boolean valueComparatorWithVariance(String valueString1, String valueString2) {
+		double value1 = Double.parseDouble(valueString1);
+		double value2 = Double.parseDouble(valueString2);
+		double actualVariance = Math.abs(value1 - value2);
+		if (actualVariance <= variance) {
+			return true;
+		} else {
+			throw new IllegalStateException(
+					"Matcher Exception the values difference is not in the specified range. Specifed variance range: "
+							+ variance + " Actual variance: " + actualVariance);
+		}
+	}
+
+	/**
+	 * Method to set up the variance
+	 * @param varianceKey
+	 * @throws IllegalArgumentException - if values are not decimal
+	 */
+	protected void setupVariance(String varianceKey) {
+		String varianceValue = System.getProperty(varianceKey);
+		if (varianceValue == null || varianceValue.isEmpty()) {
+			varianceValue = TestProperties.getProperties(varianceKey);
+			reportLog(extentTest, Status.WARNING, varianceKey + " is not provided, checking for default " + varianceKey
+					+ " value : <b>" + varianceValue + "</b>");
+		}
+		try {
+			variance = Double.parseDouble(varianceValue);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Variance Value should only contains the decimal values. But current "
+					+ varianceKey + " contains other than decimals" + varianceValue);
+		}
+	}
 
 }
